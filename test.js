d01dd4f0a44dc837b95dfc56e5582a52628598e3
Samail01 <sapug.2001@hotmail.com>
2023-10-24 12:35:34 +0200

    added test error

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
+const x = 10;
+const y = 15;
+
 function test() {
+    if(x == y) {
     console.log("test running, no issues");
+    } else {
+        console.error("numbers are not equal");
+    }
 }
 test()
